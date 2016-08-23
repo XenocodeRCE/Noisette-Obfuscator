7ba0ad4ace697f8cd70ffdb22a6c91716f909e87
XenocodeRCE <samjam5376@gmail.com>
2016-08-23 06:11:57 +0200

    Added CLI for linux/macOS users

--- Noisette/Obfuscation/ObfuscationProcess.cs
+++ Noisette/Obfuscation/ObfuscationProcess.cs
@@ -12,24 +12,33 @@ namespace Noisette.Obfuscation
 
         public static Dictionary<Tuple<MethodDef>, bool> SetObfuscateMethod;
 
+        public static NoisetteCore.Helper.ConsoleHelper CH;
+
         // Core.Property.module =
         public ObfuscationProcess(ModuleDefMD module)
         {
             _module = module;
+            CH = new NoisetteCore.Helper.ConsoleHelper();
+            CH.begin = DateTime.Now;
+            CH.Debug("Instancing Tuples and ModuleWriters");
             SetObfuscateMethod = new Dictionary<Tuple<MethodDef>, bool>();
             Core.Property.opts = new ModuleWriterOptions(_module);
-
+            CH.Debug("Analyzing module...");
             ObfuscationAnalyzer AZ = new ObfuscationAnalyzer(_module);
             AZ.PerformAnalyze();
         }
 
         public void DoObfusction()
         {
+            CH.Debug("Starting the Obfusction routine");
+
             //Renaming
             Protection.Renaming.RenamingProtection RP = new Protection.Renaming.RenamingProtection(_module);
+            CH.Debug("Processing Renaming ...");
             RP.RenameModule();
             //Inject Antitamper class
             Protection.AntiTampering.AntiTamperingProtection ATP = new Protection.AntiTampering.AntiTamperingProtection(_module);
+            CH.Debug("Processing AntiTamper ...");
             ATP.Process();
 
             //Melt Constant
@@ -43,10 +52,14 @@ namespace Noisette.Obfuscation
             //Protection.InvalidMetadata.InvalidMD.InsertInvalidMetadata(module);
             //todo : something is wrong
 
+            CH.Debug("Saving File");
+
             SaveAssembly();
 
             //post-stage antitamper
             //todo : make a proper post-process class
+
+            CH.Info("Injecting AntiTamper value");
             Protection.AntiTampering.AntiTamperingProtection.Md5(Path.GetDirectoryName(_module.Location) + @"\" + Path.GetFileNameWithoutExtension(_module.Location) + "_nutsed.exe");
         }
 
@@ -54,6 +67,8 @@ namespace Noisette.Obfuscation
         {
             Core.Property.opts.Logger = DummyLogger.NoThrowInstance;
             _module.Write(Path.GetDirectoryName(_module.Location) + @"\" + Path.GetFileNameWithoutExtension(_module.Location) + "_nutsed.exe", Core.Property.opts);
+            CH.Debug("File Saved ! ");
+            CH.Finish();
         }
     }
 }

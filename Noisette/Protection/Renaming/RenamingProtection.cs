8a1d389ecfb7c58442833efda660b47195cc2e00
Rottweiler <Rottweiler@users.noreply.github.com>
2017-09-26 12:49:36 -0500

    fixed imports

--- Noisette/Protection/Renaming/RenamingProtection.cs
+++ Noisette/Protection/Renaming/RenamingProtection.cs
@@ -4,6 +4,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Runtime.InteropServices;
+using System.IO;
 
 namespace NoisetteCore.Protection.Renaming
 {
